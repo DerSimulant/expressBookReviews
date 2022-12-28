b33aefb22a79ea375a6bd04dc17766a1adf451db
DerSimulant <82386236+DerSimulant@users.noreply.github.com>
2022-12-28 20:12:17 +0100

    Update auth_users.js
    added delete and modify

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -50,21 +50,71 @@ regd_users.post("/login", (req,res) => {
   });
 
 // Add a book review
+
 //regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
- // return res.status(300).json({message: "Yet to be implemented"});
+  //  if (req.body.isbn){
+   //     books[req.body.isbn] = {
+   //         "reviews":req.body.review,
+            
+            
+     //       }
+   // }
+  //  res.send("A review has been added!");
+//});
+
+// Define a route for adding a new book review
+
+  // Define a route for updating an existing book review
+  regd_users.put("/reviews/:isbn", (req, res) => {
+    const bookIsbn = req.params.isbn;
+  const reviewText = req.body.reviewText;
+ 
+
+  // Find the book in the books object
+  let book = books[bookIsbn];
+
+  // If the book does not exist, return an error
+  if (!book) {
+    return res.status(404).send("Book not found");
+  }
+
+   else {
+    // Update the review text for the current user
+    book.reviews = reviewText;
+  }
+
+  // Send a response to the client indicating success
+  return res.status(200).send("Book review added or modified successfully");
 });
 
-regd_users.put("/auth/review/:isbn", (req, res) => {
-    if (req.body.isbn){
-        books[req.body.isbn] = {
-            "reviews":req.body.review,
-            
-            
-            }
+// Define a route for deleting a book review
+regd_users.delete("/reviews/:isbn", (req, res) => {
+    // Retrieve the book ISBN and username from the request body and query
+    const bookIsbn = req.params.isbn;
+    
+  
+    // Find the book in the books object
+    let book = books[bookIsbn];
+  
+    // If the book does not exist, return an error
+    if (!book) {
+      return res.status(404).send("Book not found");
     }
-    res.send("A review has been added!");
-});
+  
+    // Find the review for the current user
+    let review = book.reviews;
+  
+    // If the review does not exist, return an error
+    if (!review) {
+      return res.status(404).send("Review not found");
+    }
+  
+    // Delete the review for the current user
+    delete book.reviews;
+  
+    // Send a response to the client indicating success
+    return res.status(200).send("Book review deleted successfully");
+  });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
