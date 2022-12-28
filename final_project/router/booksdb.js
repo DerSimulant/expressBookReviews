33c71190249c0a01f3c13fb24cb161de9aec3af1
DerSimulant <82386236+DerSimulant@users.noreply.github.com>
2022-12-28 14:54:06 +0100

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+    "9181238763": {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+    "9181238764": {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": "Artifical Review done by myself, otherwise {} would have been the only output, book is great ;)" },
+    "9181238765": {"author": "dante-alighieri","title": "The Divine Comedy", "reviews": {} },
+    "9181238766": {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
+    "9181238767": {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
+    "9181238768": {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
+    "9181238769": {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+    "9181238770": {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+    "9181238771": {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+    "9181238772": {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
 }
 
 module.exports=books;
